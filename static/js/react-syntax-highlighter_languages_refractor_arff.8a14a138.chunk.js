c0e45d1ea157b289593f26d94c92d39601e02dd6
Adaeze Ndupu <andupu4@gmail.com>
2022-12-03 21:11:13 +0100

    Updates

---
+++ static/js/react-syntax-highlighter_languages_refractor_arff.8a14a138.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgradient_website=self.webpackChunkgradient_website||[]).push([[1438],{7209:function(e){function t(e){e.languages.arff={comment:/%.*/,string:{pattern:/(["'])(?:\\.|(?!\1)[^\\\r\n])*\1/,greedy:!0},keyword:/@(?:attribute|data|end|relation)\b/i,number:/\b\d+(?:\.\d+)?\b/,punctuation:/[{},]/}}e.exports=t,t.displayName="arff",t.aliases=[]}}]);
+//# sourceMappingURL=react-syntax-highlighter_languages_refractor_arff.8a14a138.chunk.js.map
---
+++ static/js/react-syntax-highlighter_languages_refractor_ini.e8984672.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgradient_website=self.webpackChunkgradient_website||[]).push([[6495],{3409:function(t){function e(t){t.languages.ini={comment:/^[ \t]*[;#].*$/m,selector:/^[ \t]*\[.*?\]/m,constant:/^[ \t]*[^\s=]+?(?=[ \t]*=)/m,"attr-value":{pattern:/=.*/,inside:{punctuation:/^[=]/}}}}t.exports=e,e.displayName="ini",e.aliases=[]}}]);
+//# sourceMappingURL=react-syntax-highlighter_languages_refractor_ini.e8984672.chunk.js.map
